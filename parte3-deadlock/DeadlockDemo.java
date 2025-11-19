9e88b1b637de0957f81822b8da55015efa8b8009
sasassa123 <160864462+sasassa123@users.noreply.github.com>
2025-11-19 18:42:20 -0300

    Create DeadlockDemo.java

---
+++ parte3-deadlock/DeadlockDemo.java
@@ -0,0 +1,43 @@
+public class DeadlockDemo {
+
+    static final Object LOCK_A = new Object();
+    static final Object LOCK_B = new Object();
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(() -> {
+            synchronized (LOCK_A) {
+                System.out.println("T1: adquiriu LOCK_A");
+                dormir(100);
+
+                System.out.println("T1: tentando LOCK_B...");
+                synchronized (LOCK_B) {
+                    System.out.println("T1: concluiu (A -> B)");
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            synchronized (LOCK_B) {
+                System.out.println("T2: adquiriu LOCK_B");
+                dormir(100);
+
+                System.out.println("T2: tentando LOCK_A...");
+                synchronized (LOCK_A) {
+                    System.out.println("T2: concluiu (B -> A)");
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+    static void dormir(long ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
