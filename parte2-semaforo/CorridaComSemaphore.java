1a4db18c30e56c8323d6aae6be455fecce3f9f4c
sasassa123 <160864462+sasassa123@users.noreply.github.com>
2025-11-21 04:34:49 -0300

    Update CorridaComSemaphore.java

--- parte2-semaforo/CorridaComSemaphore.java
+++ parte2-semaforo/CorridaComSemaphore.java
@@ -2,7 +2,7 @@ import java.util.concurrent.*;
 
 public class CorridaComSemaphore {
     static int count = 0;
-    static final Semaphore sem = new Semaphore(1, true);
+    static final Semaphore sem = new Semaphore(1, true); 
     
     public static void main(String[] args) throws Exception {
         int T = 8, M = 250_000;
@@ -22,17 +22,12 @@ public class CorridaComSemaphore {
         };
         
         long t0 = System.nanoTime();
-        
-        for (int i = 0; i < T; i++) {
-            pool.submit(r);
-        }
-        
+        for (int i = 0; i < T; i++) pool.submit(r);
         pool.shutdown();
         pool.awaitTermination(1, TimeUnit.MINUTES);
-        
         long t1 = System.nanoTime();
         
         System.out.printf("Esperado=%d, Obtido=%d, Tempo=%.3fs%n",
-                         T * M, count, (t1 - t0) / 1e9);
+                T * M, count, (t1 - t0) / 1e9);
     }
 }
